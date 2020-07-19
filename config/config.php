35d130be6faed016f362c0441c189bc32627837a
Santosh Potu <santosh-potu@users.noreply.github.com>
2020-07-19 21:19:55 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
 
-define("FROM_EMAIL","santosh.potu@zee.esselgroup.com");
+define("FROM_EMAIL","santosh.potu");
 define("FROM_NAME","Santosh Kumar Potu");
 
 define("SMTP_HOST","smtp.office365.com");
 define("SMTP_PORT","587");
 
-define("SMTP_USER","santosh.potu@zee.esselgroup.com");
-define("SMTP_PWD", "Zeel2020@456");
+define("SMTP_USER","");
+define("SMTP_PWD", "");
 
